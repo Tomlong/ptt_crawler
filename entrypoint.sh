5b262a67a3cd402f912bcf0452538cee6c7fb8a4
terrylin <terrylin@emotibot.com>
2020-03-11 01:52:47 +0800

    Remove useless file

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec python3 main.py --host 0.0.0.0 --port 80
--- web/schemas.py
+++
@@ -1,6 +0,0 @@
-from schema import Schema, And
-
-CrawlerSchema = Schema({
-    "start_time": And(str, len),
-    "end_time": And(str, len)
-})
